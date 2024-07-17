4409e4c7151a9bf652345c465d4d3164fc2f938d
Suman Singh <sumansingh@Sumans-MacBook-Pro.local>
2024-07-17 15:15:07 +0530

    header is done

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,21 +1,35 @@
-"use client"
-import React,{useState} from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import MenuIconSvg from "../icons/MenuIconSvg";
-import CloseIconSvg from "../icons/CloseIconSvg";
 
 const TopHeader = () => {
-const[isOpen,setIsOpen]=useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="fixed top-0 left-0 w-full  bg-white  px-4 lg:px-0 z-50">
-      <div className="max-w-[1024px] mx-auto flex justify-between items-center h-20 ">
-      <MenuIconSvg />
-        {/* <div className="flex items-center">
-          {isOpen ?<CloseIconSvg/> :<MenuIconSvg />}
-         
-        </div> */}
- <Image
+    <header className="fixed top-0 left-0 w-full  bg-white px-5 sm:px-4 lg:px-0 z-50">
+      <div className="max-w-[1024px] mx-auto flex justify-between items-center h-[60px] sm:h-20 ">
+        <div className="flex items-center">
+          {isOpen ? (
+            <Image
+              src="/assets/close.png"
+              alt="Example Image"
+              width={30}
+              height={10}
+              className="cursor-pointer sm:hidden h-8"
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          ) : (
+            <Image
+              src="/assets/more.png"
+              alt="Example Image"
+              width={30}
+              height={10}
+              className="cursor-pointer sm:hidden h-8"
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          )}
+
+          <Image
             src={
               "https://dqxth8lmt6m4r.cloudfront.net/assets/v1/Logo-1b7ad53fafc81e97cf23af4f0ffae8d8961ee793cd3d982c132b3acfa8b75bdd.svg"
             }
@@ -24,23 +38,44 @@ const[isOpen,setIsOpen]=useState(false)
             height={40}
             className="cursor-pointer ml-4 sm:ml-0"
           />
-        <div className="flex justify-between items-center text-[#112D35] font-semibold">
-          <div className="hidden sm:flex justify-between ">
-            <Link href="/#">About Us</Link>
-            <Link href="/#" className="ml-6">
+        </div>
+        {/* that is for mobile */}
+        {isOpen && (
+          <div className="sm:hidden absolute left-0 top-[60px] flex flex-col bg-white w-full py-4 px-5 text-[#112D35] font-semibold">
+            <Link href="/#" className="mb-4 py-2">
+              About Us
+            </Link>
+            <Link href="/#" className="mb-4 py-2">
               Blog
             </Link>
-            <Link href="/#" className="ml-6">
+            <Link href="/#" className="mb-4 py-2">
               FAQ
             </Link>
-            <Link href="/#" className="ml-6">
+            <Link href="/#" className="mb-4 py-2">
               Contact Us
             </Link>
-            <Link href="/#" className="ml-6">
+            <Link href="/#" className="mb-4 py-2">
               Careers
             </Link>
           </div>
-          <button className="w-[120px] h-[35px] md:w-[130px] md:h-[40px] rounded-full bg-[#1F94AA] text-white cursor-pointer ml-6">
+        )}
+        <div className="flex justify-between items-center text-[#112D35] font-semibold">
+          <div className="hidden sm:flex justify-between">
+            <Link href="/#">About Us</Link>
+            <Link href="/#" className="sm:ml-4 md:ml-6">
+              Blog
+            </Link>
+            <Link href="/#" className="sm:ml-4 md:ml-6">
+              FAQ
+            </Link>
+            <Link href="/#" className="sm:ml-4 md:ml-6">
+              Contact Us
+            </Link>
+            <Link href="/#" className="sm:ml-4 md:ml-6">
+              Careers
+            </Link>
+          </div>
+          <button className="w-[110px] h-[35px] sm:w-[120px] sm:h-[35px] rounded-full bg-[#1F94AA] text-white text-[13px] sm:text-sm cursor-pointer ml-6">
             Try Inito
           </button>
         </div>
